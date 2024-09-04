dcc8df4744df3de0ffa6633b0832db76bd915ef8
Natalia Tsvetkova <ntsvetko@c3r3s2.42prague.com>
2024-09-04 20:00:52 +0200

    im tired

--- cmds.c
+++ cmds.c
@@ -2,25 +2,76 @@
 
 t_cmd *cmd_new()
 {
-    t_cmd *cmd = malloc(sizeof(t_cmd));
-    cmd->args = NULL;
-    cmd->input = NULL;
-    cmd->output = NULL;
-    cmd->append = NULL;
-    cmd->delimiter = NULL;
-    cmd->next = NULL;
-    return cmd;
+	t_cmd *cmd;
+	
+	cmd = malloc(sizeof(t_cmd));
+	cmd->args = NULL;
+	cmd->input = NULL;
+	cmd->output = NULL;
+	cmd->append = NULL;
+	cmd->delimiter = NULL;
+	cmd->next = NULL;
+	return (cmd);
 }
 
-void add_cmd_arg(t_cmd *cmd, char *arg)
+void	free_arr(char **arr)
 {
-    int count = 0;
-    if (cmd->args)
-        while (cmd->args[count])
-			count++;
-    cmd->args = realloc(cmd->args, sizeof(char *) * (count + 2));
-    cmd->args[count] = ft_strdup(arg);
-    cmd->args[count + 1] = NULL;
+	int i;
+
+	i = 0;
+	while (arr[i])
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr);
+}
+
+void	add_cmd_arg(t_cmd *cmd, char *arg)
+{
+	char	**tmp;
+	int		i;
+	int		j;
+	
+	i = 0;
+	j = 0;
+	if (cmd->args)
+	{
+		while (cmd->args[i])
+			i++;
+		tmp = (char **)malloc(sizeof(char *) * (i + 1));
+		if (!tmp)
+			return ;
+		while (j < i)
+		{	
+			tmp[j] = cmd->args[j];
+			j++;
+		}
+		free(cmd->args);
+		cmd->args = (char **)malloc(sizeof(char *) * (i + 2));
+		if (!cmd->args)
+		{
+			free(tmp);
+			return ;
+		}
+		j = 0;
+		while ( j < i)
+		{
+			cmd->args[j] = tmp[j];
+			j++;
+		}
+		cmd->args[i] = ft_strdup(arg);
+		cmd->args[i + 1] = NULL;
+		free(tmp);
+	}
+	else
+	{
+		cmd->args = (char **)malloc(sizeof(char *) * 2);
+		if (!cmd->args)
+			return ;
+		cmd->args[0] = ft_strdup(arg);
+		cmd->args[1] = NULL;
+	}
 }
 
 t_cmd *parse_tokens(t_token *tokens) 
@@ -44,7 +95,6 @@ t_cmd *parse_tokens(t_token *tokens)
 	{
 		if (tokens->type == WORD)
 		{
-			
 			add_cmd_arg(current_cmd, tokens->txt);
 			arg_count++;
 		}
@@ -67,7 +117,8 @@ t_cmd *parse_tokens(t_token *tokens)
 		else if (tokens->type == APPEND)
 		{
 			if (tokens->next->type != WORD)
-				printf("no.\n");tokens = tokens->next;
+				printf("no.\n");
+			tokens = tokens->next;
 			if (tokens && tokens->type == WORD)
 				current_cmd->append = ft_strdup(tokens->txt);
 
