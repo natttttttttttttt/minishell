5bb1f2dcbf8cd96b3e6de323747e7d02934487b4
Natalia Tsvetkova <ntsvetko@c2r6s2.42prague.com>
2024-09-30 14:39:52 +0200

    main norm

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int	all_spaces(char *str)
 	return (1);
 }
 
-int	parsing(char *str)
+int	parsing_ok(char *str)
 {
 	if (!str)
 		return (0);
@@ -26,103 +26,35 @@ int	parsing(char *str)
 	return (1);
 }
 
-void	vars_to_value(t_token *lst, int i, int start, t_info info)
-{
-	char	*s;
-	char	*v;
-	char	*tmp;
-	char	*env_val;
 
-	while (lst)
+void	print_cmd_lst(t_cmd *cmd_lst)
+{
+	t_cmd	*cmd;
+	int		i;
+
+	cmd = cmd_lst;
+	i = 0;
+	while (cmd)
 	{
-		if (lst->type == VAR)
+		printf("Command:\n");
+		while (cmd->args[i])
 		{
-			if (ft_strncmp(lst->txt, "$?", 3) == 0)
-			{
-				free(lst->txt);
-				lst->txt = ft_itoa(info.exit_code);
-				lst->type = WORD;
-				lst = lst->next;
-				continue ;
-			}
-			s = ft_strdup("");
-			i = 0;
-			start = 0;
-			while ((lst->txt)[i])
-			{
-				if ((lst->txt)[i] == '$')
-				{
-					if (i != start)
-					{
-						tmp = malloc(sizeof(char) * (i - start + 1));;
-						ft_strncpy(tmp, lst->txt + start, i - start);
-						s = ft_strjoin(s, tmp);
-						free(tmp);
-					}
-					start = ++i;
-					while ((lst->txt)[i] && (ft_isalnum((lst->txt)[i])
-							|| (lst->txt)[i] == '_'))
-						i++;
-					v = malloc(sizeof(char) * (i - start + 1));
-					ft_strncpy(v, lst->txt + start, i - start);
-					env_val = ft_getenv(info.my_envp, v);
-					free(v);
-					tmp = s;
-					if (env_val)
-						s = ft_strjoin(tmp, env_val);
-					free(tmp);
-					start = i;
-				}
-				else
-					i++;
-			}
-			if (i != start)
-			{
-				tmp = malloc(sizeof(char) * (i - start + 1));
-				ft_strncpy(tmp, lst->txt + start, i - start);
-				s = ft_strjoin(s, tmp);
-				free(tmp);
-			}
-			free(lst->txt);
-			if (!env_val)
-				lst->txt = ft_strdup("");
-			else
-				lst->txt = ft_strdup(s);
-			free(s);
-			lst->type = WORD;
+			printf("  Arg[%d]: %s\n", i, cmd->args[i]);
+			i++;
 		}
-		lst = lst->next;
+		if (cmd->input)
+			printf("  Input File: %s\n", cmd->input);
+		if (cmd->output)
+			printf("  Output File: %s\n", cmd->output);
+		if (cmd->append)
+			printf("  Append File: %s\n", cmd->append);
+		if (cmd->delimiter)
+			printf("  Heredoc Delimiter: %s\n", cmd->delimiter);
+		cmd = cmd->next;
+		if (cmd)
+			printf("  |\n");
 	}
 }
-
-// void	print_cmd_lst(t_cmd *cmd_lst)
-// {
-// 	t_cmd	*cmd;
-// 	int		i;
-
-// 	cmd = cmd_lst;
-// 	i = 0;
-// 	while (cmd)
-// 	{
-// 		printf("Command:\n");
-// 		while (cmd->args[i])
-// 		{
-// 			printf("  Arg[%d]: %s\n", i, cmd->args[i]);
-// 			i++;
-// 		}
-// 		if (cmd->input)
-// 			printf("  Input File: %s\n", cmd->input);
-// 		if (cmd->output)
-// 			printf("  Output File: %s\n", cmd->output);
-// 		if (cmd->append)
-// 			printf("  Append File: %s\n", cmd->append);
-// 		if (cmd->delimiter)
-// 			printf("  Heredoc Delimiter: %s\n", cmd->delimiter);
-// 		cmd = cmd->next;
-// 		if (cmd)
-// 			printf("  |\n");
-// 	}
-// }
 
 void	info_init(t_info *info, char **envp)
 {
@@ -133,6 +65,27 @@ void	info_init(t_info *info, char **envp)
 	info->exit_code = 0;
 	info->tokens = NULL;
 	info->cmds = NULL;
+}
+
+void	parse_and_exe(t_info *info, t_cmd *cmd_lst, t_token *token_lst)
+{
+	add_history(info->input);
+	if (save_tokens(info->input, &token_lst, info))
+	{
+		info->tokens = token_lst;
+		vars_to_value(token_lst, 0, 0, *info);
+		cmd_lst = parse_tokens(token_lst, info);
+		print_cmd_lst(cmd_lst);
+		info->cmds = cmd_lst;
+		if (cmd_lst)
+		{
+			cmd_to_path(cmd_lst, info);
+			execute_commands(cmd_lst, info);
+			free_command_list(cmd_lst);
+		}
+		free_token_lst(token_lst);
+	}
+	free(info->input);
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -147,32 +100,16 @@ int	main(int argc, char **argv, char **envp)
 	while (1)
 	{
 		token_lst = NULL;
-		
 		info.input = readline("minishell> ");
 		if (!info.input)
 		{
 			printf("exit\n");
 			break ;
 		}
-		else if (parsing(info.input))
+		else if (parsing_ok(info.input))
 		{
-			add_history(info.input);
-			if (save_tokens(info.input, &token_lst, &info))
-			{
-				info.tokens = token_lst;
-				vars_to_value(token_lst, 0, 0, info);
-				cmd_lst = NULL;
-				cmd_lst = parse_tokens(token_lst, &info);
-				info.cmds = cmd_lst;
-				if (cmd_lst)
-				{
-					cmd_to_path(cmd_lst, &info);
-					execute_commands(cmd_lst, &info);
-					free_command_list(cmd_lst);
-				}
-				free_token_lst(token_lst);
-			}
-			free(info.input);
+			cmd_lst = NULL;
+			parse_and_exe(&info, cmd_lst, token_lst);
 		}
 	}
 	free_arr(info.paths);
