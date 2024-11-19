4ff4d8e8cc386ab917a9274f060970a6dc9b9010
pibouill <pibouill@student.42prague.com>
2024-11-19 11:40:09 +0100

    commands test script

---
+++ dirs_test.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    dirs_test.sh                                       :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: pibouill <pibouill@student.42prague.com>   +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/11/19 11:36:25 by pibouill          #+#    #+#              #
+#    Updated: 2024/11/19 11:36:29 by pibouill         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+
+# Array of commands
+commands=(
+    "minishell.h"
+    "minishell_tester/"
+    "minishell_tester"
+    "/minishell_tester"
+    "/minishell_tester/"
+	"./minishell_tester"
+	"./minishell_tester/"
+	"bla"
+	"/bla"
+	"bla/"
+	"./bla"
+	"./bla/"
+
+)
+
+# Loop through each command
+for cmd in "${commands[@]}"; do
+    echo "Running: $cmd"
+    eval $cmd
+    exit_code=$?
+
+    # Check exit code
+    if [ $exit_code -eq 0 ]; then
+        echo "Command succeeded with exit code $exit_code"
+    else
+        echo "Command failed with exit code $exit_code"
+    fi
+    echo "----------------------------------"
+done
+
