64dc6adb4634dbf1c776685d5778c26d2e99a108
pibouill <pibouill@student.42prague.com>
2025-01-08 15:41:38 +0100

    norminette unset_builtin

--- src/unset_builtin.c
+++ src/unset_builtin.c
@@ -12,12 +12,96 @@
 
 #include "../inc/minishell.h"
 
+// int	unset_builtin(char **args, t_info *info, int i, int j)
+// {
+// 	int		n;
+// 	int		k;
+// 	int		remove;
+// 	char	**new_env;
+//
+// 	if (args[1] == NULL)
+// 		return (0);
+// 	while (args[i] && find_env_var(info->my_envp, args[i]) != -1)
+// 	{
+// 		j++;
+// 		i++;
+// 	}
+// 	n = 0;
+// 	while (info->my_envp[n] != NULL)
+// 		n++;
+// 	new_env = malloc(sizeof(char *) * (n + 1 - j));
+// 	if (!new_env)
+// 	{
+// 		perror("malloc");
+// 		return (errno);
+// 	}
+// 	i = 0;
+// 	j = 0;
+// 	while (i < n)
+// 	{
+// 		remove = 0;
+// 		k = 1;
+// 		while (args[k])
+// 		{
+// 			if (ft_strncmp(info->my_envp[i], args[k], ft_strlen(args[k])) == 0
+// 				&& info->my_envp[i][ft_strlen(args[k])] == '=')
+// 			{
+// 				remove = 1;
+// 				break ;
+// 			}
+// 			k++;
+// 		}
+// 		if (!remove)
+// 			new_env[j++] = info->my_envp[i];
+// 		i++;
+// 	}
+// 	new_env[j] = NULL;
+// 	free(info->my_envp);
+// 	info->my_envp = new_env;
+// 	return (0);
+// }
+
+int	should_remove_var(char *env_var, char **args)
+{
+	int	k;
+
+	k = 1;
+	while (args[k])
+	{
+		if (ft_strncmp(env_var, args[k], ft_strlen(args[k])) == 0
+			&& env_var[ft_strlen(args[k])] == '=')
+			return (1);
+		k++;
+	}
+	return (0);
+}
+
+char	**create_filtered_env(char **old_env, char **args,
+		int size, int remove_count)
+{
+	char	**new_env;
+	int		i;
+	int		j;
+
+	new_env = malloc(sizeof(char *) * (size + 1 - remove_count));
+	if (!new_env)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (i < size)
+	{
+		if (!should_remove_var(old_env[i], args))
+			new_env[j++] = old_env[i];
+		i++;
+	}
+	new_env[j] = NULL;
+	return (new_env);
+}
+
 int	unset_builtin(char **args, t_info *info, int i, int j)
 {
-	int		n;
-	int		k;
-	int		remove;
 	char	**new_env;
+	int		env_size;
 
 	if (args[1] == NULL)
 		return (0);
@@ -26,36 +110,12 @@ int	unset_builtin(char **args, t_info *info, int i, int j)
 		j++;
 		i++;
 	}
-	n = 0;
-	while (info->my_envp[n] != NULL)
-		n++;
-	new_env = malloc(sizeof(char *) * (n + 1 - j));
+	env_size = 0;
+	while (info->my_envp[env_size])
+		env_size++;
+	new_env = create_filtered_env(info->my_envp, args, env_size, j);
 	if (!new_env)
-	{
-		perror("malloc");
-		return (errno);
-	}
-	i = 0;
-	j = 0;
-	while (i < n)
-	{
-		remove = 0;
-		k = 1;
-		while (args[k])
-		{
-			if (ft_strncmp(info->my_envp[i], args[k], ft_strlen(args[k])) == 0
-				&& info->my_envp[i][ft_strlen(args[k])] == '=')
-			{
-				remove = 1;
-				break ;
-			}
-			k++;
-		}
-		if (!remove)
-			new_env[j++] = info->my_envp[i];
-		i++;
-	}
-	new_env[j] = NULL;
+		return (perror("malloc"), errno);
 	free(info->my_envp);
 	info->my_envp = new_env;
 	return (0);
--- to_norminette.md
+++ to_norminette.md
@@ -1,15 +1,19 @@
-cmd_to_path.c -> line too long on commented previous functions 
+- [x] cmd_to_path.c -> line too long on commented previous functions 
 
-cmds.c -> add_cmd_arg() and parse_tokens() => too long functions
+- [ ] cmds.c -> add_cmd_arg() and parse_tokens() => too long functions
 
-envp.c -> copy_env() and update_env() => too long
+- [ ] envp.c -> copy_env() and update_env() => too long
 
-export_builtins.c -> func too long
+- [ ] export_builtins.c -> func too long
 
-heredoc.c -> func too long
+- [ ] heredoc.c -> func too long
 
-main.c -> parse_and_exe() => too long bc of the debugging stuff, keeping it for now just in case
+- [ ] main.c -> parse_and_exe() => too long bc of the debugging stuff, keeping it for now just in case
 
-print_stuff.c -> lots of stuff but ig we dc
+- [ ] print_stuff.c -> lots of stuff but ig we dc
 
-unset_builtin.c -> func too long
+- [x] unset_builtin.c -> func too long
+
+---
+
+tester result -> 107
